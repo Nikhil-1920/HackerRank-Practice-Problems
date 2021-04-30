db6c816879409cb7e644352c00c1d8bd42118ee5
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2021-04-30 10:24:50 +0530

    Create 1. "Hello World!" in C.c

---
+++ C/Introduction/1. "Hello World!" in C.c
@@ -0,0 +1,12 @@
+// "Hello World!" in C
+#include <stdio.h>
+
+int main(void) {
+    char s[100];
+
+    scanf("%[^\n]%*c", s);
+    printf("Hello, World!\n");
+    printf("%s\n", s);
+
+    return 0;
+}
