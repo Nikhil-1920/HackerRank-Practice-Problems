70e0735bddc0757a49b985b7e35676eb668c216e
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2021-05-01 10:57:07 +0530

    Update 10. Compute The Average.sh

--- Linux Shell/Bash/10. Compute The Average.sh
+++ Linux Shell/Bash/10. Compute The Average.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 read n
-sum=0
+sum = 0
 for ((i = 0; i < n; i++));
 do
-read var
-sum=$((sum + var))
+  read var
+  sum = $((sum + var))
 done
-printf "%.3f" `echo "$sum/$n" | bc -l`
+printf "%.3f" `echo "$sum/$n" | bc -l
